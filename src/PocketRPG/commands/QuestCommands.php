b9a88fd4c8afd634952179aa6f2b88307a4005ca
Blabla <st.ten.veldhuis@gmail.com>
2016-08-17 16:49:39 +0200

    Create QuestCommands.php

---
+++ src/PocketRPG/commands/QuestCommands.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PocketRPG\commands;
+
+use PocketRPG\Main;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+
+class QuestCommands extends PluginBase implements Listener {
+  
+  public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
+  
+  }
+}
