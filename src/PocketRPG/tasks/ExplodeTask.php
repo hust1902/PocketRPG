bdb0137b2384915ddcef034df3d47d274de51847
Blabla <st.ten.veldhuis@gmail.com>
2016-08-17 11:27:09 +0200

    Adding ExplodeTask for Warrior ability

---
+++ src/PocketRPG/tasks/ExplodeTask.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace FWAcoreclasses;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\math\Vector3;
+use pocketmine\level\Position;
+use pocketmine\level\particle\HugeExplodeParticle;
+
+class ExplodeTask extends PluginTask implements Listener{
+  
+  public function __construct(Main $plugin) {
+    parent::__construct($plugin);
+    $this->plugin = $plugin;
+  }
+  
+  public function getPlugin() {
+    return $this->plugin;
+  }
+  
+  public function onRun($tick) {
+    foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p);
+    $cause = $p->getLastDamageCause();
+    if($cause instanceof EntityDamageByEntityEvent){
+      if($cause->getDamager() instanceof Player and $cause->getDamager()->hasPermission("class.warrior") and $cause->getDamager()->getItemInHand()->getId() == Item::IRON_SHOVEL) {
+          $damager = $p->getLastDamageCause()->getDamager();
+          $hit = $p;
+          $damager->getLevel()->addParticle(new HugeExplodeParticle(new Vector3($hit->x, $hit->y, $hit->z)));
+          $hit->attack(6, EntityDamageEvent::CAUSE_ENTITY_ATTACK);
+      }
+    }
+  }
+}
