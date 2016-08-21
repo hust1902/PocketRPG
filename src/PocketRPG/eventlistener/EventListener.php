3d94490dd75852bde78318d9eb4e60365b396099
Blabla <st.ten.veldhuis@gmail.com>
2016-08-21 23:44:34 +0200

    Update EventListener.php

--- src/PocketRPG/eventlistener/EventListener.php
+++ src/PocketRPG/eventlistener/EventListener.php
@@ -42,7 +42,6 @@ class EventListener extends PluginBase implements Listener {
 
   public function __construct(Main $plugin) {
     $this->plugin = $plugin;
-    $this->config = $config;
     $this->config = $this->plugin->config;
   }
   
