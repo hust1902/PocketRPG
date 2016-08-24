b29b44558abee38f5afe04c6a98d794bbd83eb4c
Blabla <st.ten.veldhuis@gmail.com>
2016-08-24 19:40:10 +0200

    Update EventListener.php

--- src/PocketRPG/eventlistener/EventListener.php
+++ src/PocketRPG/eventlistener/EventListener.php
@@ -44,7 +44,7 @@ class EventListener extends PluginBase implements Listener {
   public function __construct(Main $plugin) {
     $this->plugin = $plugin;
     $this->config = $this->plugin->config;
-    $this->config = $config;
+    $config = $this->config;
   }
   
   public function onFight(EntityDamageEvent $event) {
