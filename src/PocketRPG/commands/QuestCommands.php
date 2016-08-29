df14b0086c28770ca93a7fc0694c0b7c34107fea
Blabla <st.ten.veldhuis@gmail.com>
2016-08-29 14:19:59 +0200

    Add /quest edit ...

--- src/PocketRPG/commands/QuestCommands.php
+++ src/PocketRPG/commands/QuestCommands.php
@@ -47,44 +47,78 @@ class QuestCommands extends PluginBase implements CommandExecutor{
         case "edit":
           $quest = new Config ($this->getDataFolder () . "quests/" . $args [1] . ".yml");
           if($p->hasPermission("quests.command") || $p->hasPermission("quests.command.edit")) {
-            if(isset($args[3]) && is_numeric($args[1])) {
+            if(isset($args[3]) && is_numeric($args[1]) && is_numeric ($args [3])) {
               switch(strtolower($args[2])) {
                 
                 case "questname":
                   if (file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml")) {
                     $questname = array_shift (array_shift (array_shift ($args)));
                     $quest->set ("QuestName", implode (" ", $questname));
+                    $p->sendMessage (TF::GREEN . "You succesfully changed the quest name of " . $args [1] . " to " . $questname . ".");
+                  } else {
+                    $p->sendMessage (TF::RED . "There is no quest with that quest ID!");
                   }
                 return true;
                 break;
                 
                 case "questdescription":
-
+                  if (file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml")) {
+                    $questdescription = array_shift (array_shift (array_shift ($args)));
+                    $quest->set ("QuestDescription", implode (" ", $questdescription));
+                    $p->sendMessage (TF::GREEN . "You succesfully changed the quest description of " . $args [1] . " to " . $questdescription . ".");
+                  } else {
+                    $p->sendMessage (TF::RED . "There is no quest with that quest ID!");
+                  }
                 return true;
                 break;
                 
                 case "requiredid":
-
+                  if (file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml") && is_numeric ($args [3])) {
+                    $quest->set ("RequiredID", $args [3]);
+                    $p->sendMessage (TF::GREEN . "You succesfully changed the required ID of " . $args [1] . " to " . $args[3] . ".");
+                  } else {
+                    $p->sendMessage (TF::RED . "There is no quest with that quest ID!");
+                  }
                 return true;
                 break;
                 
                 case "requiredamount":
+                  if (file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml") && is_numeric ($args [3])) {
+                    $quest->set ("RequiredAmount", $args [3]);
+                    $p->sendMessage (TF::GREEN . "You succesfully changed the required ID of " . $args [1] . " to " . $args[3] . ".");
+                  } else {
+                    $p->sendMessage (TF::RED . "There is no quest with that quest ID!");
+                  }
                 return true;
                 break;
                 
                 case "rewardid":
+                  if (file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml") && is_numeric ($args [3])) {
+                    $quest->set ("RewardID", $args [3]);
+                    $p->sendMessage (TF::GREEN . "You succesfully changed the reward ID of " . $args [1] . " to " . $args[3] . ".");
+                  } else {
+                    $p->sendMessage (TF::RED . "There is no quest with that quest ID!");
+                  }
                 return true;
                 break;
                 
                 case "rewardamount":
+                  if (file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml") && is_numeric ($args [3])) {
+                    $quest->set ("RewardAmount", $args [3]);
+                    $p->sendMessage (TF::GREEN . "You succesfully changed the reward amount of " . $args [1] . " to " . $args[3] . ".");
+                  } else {
+                    $p->sendMessage (TF::RED . "There is no quest with that quest ID!");
+                  }
                 return true;
                 break;
                   
-                case "finishmsg":
-                return true;
-                break;
-                
-                case "cantfinishmsg":
+                case "requiredexp":
+                  if (file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml") && is_numeric ($args [3])) {
+                    $quest->set ("RequiredExp", $args [3]);
+                    $p->sendMessage (TF::GREEN . "You succesfully changed the required ID of " . $args [1] . " to " . $args[3] . ".");
+                  } else {
+                    $p->sendMessage (TF::RED . "There is no quest with that quest ID!");
+                  }
                 return true;
                 break;
               }
