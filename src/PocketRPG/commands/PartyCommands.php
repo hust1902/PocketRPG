6cec26a6cea569de1cd801fc419cb1ded740df90
Blabla <st.ten.veldhuis@gmail.com>
2016-09-02 17:57:34 +0200

    start quest commands ...

--- src/PocketRPG/commands/PartyCommands.php
+++ src/PocketRPG/commands/PartyCommands.php
@@ -14,5 +14,40 @@ use pocketmine\Player;
 use pocketmine\Server;
 
 class PartyCommands extends PluginBase implements CommandExecutor {
-
+  
+  public $plugin;
+  
+  public function __construct(Main $plugin) {
+    $this->plugin = $plugin;
+  }
+  public function getPlugin() {
+    return $this->plugin;
+  }
+  
+  public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
+    if($cmd->getName() == "party") {
+      switch(strtolower($args[0])) {
+        
+        case "invite":
+          
+        return true;
+        break;
+        
+        case "accept":
+          
+        return true;
+        break;
+        
+        case "reject":
+          
+        return true;
+        break;
+        
+        case "leave":
+          
+        return true;
+        break;
+      }
+    }
+  }
 }
