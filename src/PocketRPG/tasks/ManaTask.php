12874d10701e08fdeb63678917b7905232e8f5b9
Blabla <st.ten.veldhuis@gmail.com>
2016-09-11 18:26:46 +0200

    Finish mana system ...

---
+++ src/PocketRPG/tasks/ManaTask.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PocketRPG\tasks;
+
+use pocketmine\plugin\PluginTask;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+class ManaTask extends PluginTask {
+
+  public function __construct(Main $plugin) {
+    parent::__construct ($plugin);
+    $this->plugin = $plugin;
+  }
+
+  public function getPlugin () {
+    return $this->plugin;
+  }
+
+  public function onRun ($currentTick) {
+    foreach ($this->getPlugin()->getServer ()->getOnlinePlayers () as $p) {
+      if($p->getLevel ()->getName () == $this->getPlugin ()->config->get ("RPGworld")) {
+        if ($p->getFood () == 20) {
+          $p->setFood (20);
+        } else {
+          $p->setFood ($p->getFood() + 1);
+        }
+      }
+    }
+  }
+}
