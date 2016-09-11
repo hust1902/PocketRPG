9f39906e98488bb8ccb6f639e86ead495553ee0d
Blabla <st.ten.veldhuis@gmail.com>
2016-09-11 18:34:45 +0200

    Whoops

--- src/PocketRPG/Main.php
+++ src/PocketRPG/Main.php
@@ -30,12 +30,11 @@ class Main extends PluginBase implements Listener {
     $this->getCommand("rpg")->setExecutor(new RpgCommands($this));
     $this->getCommand("quest")->setExecutor(new QuestCommands($this));
     $this->getCommand("party")->setExecutor(new PartyCommands($this));
-    $this->getServer ()->getScheduler->scheduleRepeatingTask (new ManaTask($this), 20);
+    $this->getServer ()->getScheduler()->scheduleRepeatingTask (new ManaTask($this), 20);
     
     @mkdir($this->getDataFolder());
     $this->saveResource("config.yml");
     $this->config = new Config($this->getDataFolder(). "config.yml", Config::YAML);
-    $config = $this->config->getAll();
   }
 
   public function onDisable() {
