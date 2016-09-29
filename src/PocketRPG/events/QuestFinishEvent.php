06b3d317eac435142722cfee0963f0821607db29
Blabla <st.ten.veldhuis@gmail.com>
2016-09-29 19:15:18 +0200

    Add some more...

--- src/PocketRPG/events/QuestFinishEvent.php
+++ src/PocketRPG/events/QuestFinishEvent.php
@@ -11,14 +11,20 @@ class QuestFinishEvent extends PluginEvent implements Cancellable {
 
   private $plugin;
   private $p;
+  private $questid;
 
-  public function __construct(Main $plugin, Player $p) {
+  public function __construct(Main $plugin, Player $p, $questid) {
     $this->player = $p;
     $this->plugin = $plugin;
+    $this->questid = $questid;
     parent::__construct($plugin);
   }
 
   public function getPlayer() {
     return $this->player;
+  }
+
+  public function getQuestId() {
+    return $this->questid;
   }
 }
