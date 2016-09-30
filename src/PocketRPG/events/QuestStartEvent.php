d01a4d785d91d79df3e4a4593f7ebbf1620822db
Blabla <st.ten.veldhuis@gmail.com>
2016-09-30 18:49:51 +0200

    Continue QuestStartEvent ...

--- src/PocketRPG/events/QuestStartEvent.php
+++ src/PocketRPG/events/QuestStartEvent.php
@@ -1,1 +1,30 @@
 <?php
+
+namespace PocketRPG\events;
+
+use pocketmine\plugin\PluginEvent;
+use pocketmine\event\Cancellable;
+use pocketmine\Player;
+use PocketRPG\Main;
+
+class QuestStartEvent extends PluginEvent implements Cancellable {
+
+  private $plugin;
+  private $p;
+  private $questid;
+
+  public function __construct(Main $plugin, Player $p, $questid) {
+    $this->player = $p;
+    $this->plugin = $plugin;
+    $this->questid = $questid;
+    parent::__construct($plugin);
+  }
+
+  public function getPlayer() {
+    return $this->player;
+  }
+
+  public function getQuestId() {
+    return $this->questid;
+  }
+}
