6d02e5d9826ba6f0cab880f1322349d540a4b8dc
Blabla <st.ten.veldhuis@gmail.com>
2016-10-05 13:59:09 +0200

    This shouldnt be there...

--- src/PocketRPG/Main.php
+++ src/PocketRPG/Main.php
@@ -93,7 +93,7 @@ class Main extends PluginBase implements Listener {
   public function clearAllQuests(Player $p) {
     $quests = @scandir($this->getDataFolder() . "quests/");
     foreach($quests as $quest) {
-      $this->quest = new Config($this->getDataFolder() . "quests/" . $quest . ".yml");
+      $this->quest = new Config($this->getDataFolder() . "quests/" . $quest);
       $this->quest->remove("Finished", $p->getName());
       $this->quest->remove("Started", $p->getName());
       $this->quest->save();
