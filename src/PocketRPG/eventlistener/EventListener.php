1a7434d615ada6e1c8a9b7bdcc14848eb2c75219
Blabla <st.ten.veldhuis@gmail.com>
2016-10-08 17:37:07 +0200

    Update EventListener.php

--- src/PocketRPG/eventlistener/EventListener.php
+++ src/PocketRPG/eventlistener/EventListener.php
@@ -438,10 +438,16 @@ class EventListener extends Main implements Listener {
       foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p2) {
         if($p2->getLevel()->getName() == $this->getOwner->config->get("RPGworld")) {
           if($p->distance($p2) <= 50) {
-            $p2->sendMessage($p->getName() . TF::GRAY . " / Lvl" . $p->getExpLevel() . TF::GRAY . $this->getOwner()->playerclass->get ($p->getName()) . TF::GRAY . " > " . TF::WHITE . $m);
-          }
+            $event->setFormat($p->getName() . TF::GRAY . " / Lvl" . $p->getExpLevel() . TF::GRAY . $this->getOwner()->playerclass->get ($p->getName()) . TF::GRAY . " > " . TF::WHITE . $m);
+          } elseif($p->distance($p2) <= 100) {
+            $event->setFormat($p->getName() . TF::GRAY . " / Lvl" . $p->getExpLevel() . TF::GRAY . $this->getOwner()->playerclass->get ($p->getName()) . TF::GRAY . " > " . TF::GRAY . $m);
+          } elseif($p->distance($p2) <= 100) {
+            $event->setFormat($p->getName() . TF::GRAY . " / Lvl" . $p->getExpLevel() . TF::GRAY . $this->getOwner()->playerclass->get ($p->getName()) . TF::GRAY . " > " . TF::DARK_GRAY . $m);
+          } else {
+            $event->setCancelled();
+        } else {
+          $event->setCancelled();
         }
-      $event->setCancelled();
       }
     }
   }
