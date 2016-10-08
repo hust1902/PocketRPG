c50a8861bf1c8e6d295036a57dc39ea891be84fb
Blabla <st.ten.veldhuis@gmail.com>
2016-10-08 20:09:07 +0200

    Wow I'm so blind

--- src/PocketRPG/commands/QuestCommands.php
+++ src/PocketRPG/commands/QuestCommands.php
@@ -178,9 +178,9 @@ class QuestCommands extends PluginBase implements CommandExecutor{
         case "start":
           if (isset ($args [1]) && file_exists ($this->getDataFolder () . "quests/" . $args [1] . ".yml")) {
             $quest = new Config ($this->getDataFolder () . "quests/" . $args [1] . ".yml");
-            if ($this->getOwner()->hasQuestFinished($p, $args[1])) {
+            if($this->getOwner()->hasQuestFinished($p, $args[1])) {
               $p->sendMessage (TF::RED . "You have already finished this quest!");
-            } elseif($this->getOwner()->hasQuestFinished($p, $args[1])) {
+            } elseif($this->getOwner()->hasQuestStarted($p, $args[1])) {
               $p->sendMessage (TF::RED . "You have already started this quest!");
             } elseif($p->getExpLevel () >= $quest->get ("RequiredExpLvl")) {
               $this->getOwner()->startQuest($p, $args[1]);
