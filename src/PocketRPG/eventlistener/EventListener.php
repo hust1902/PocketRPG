8299413a8a5d3176a0b715aa802689f76d260c34
Blabla <st.ten.veldhuis@gmail.com>
2016-10-15 16:22:16 +0200

    Update EventListener.php

--- src/PocketRPG/eventlistener/EventListener.php
+++ src/PocketRPG/eventlistener/EventListener.php
@@ -71,7 +71,7 @@ class EventListener extends Main implements Listener {
   public function onInteract() {
     $p = $event->getPlayer();
       if($p->getItemInHand()->getId() == Item::BLAZE_POWDER && $this->getOwner()->playerclass->get($p->getName() == "mage")) {
-        foreach($this->getServer()->getOnlinePlayers() as $ps) {
+        foreach($this->getOwner()->getServer()->getOnlinePlayers() as $ps) {
           if($p->distance($ps) <= 10 && $p->getName() != $ps->getName() && $ps->getLevel()->getName() == $this->getOwner()->config->get("RPGworld") && $p->getFood() >= 9) {
             $p->setFood($p->getFood() - 9);
             $pos = $ps->getPosition();
