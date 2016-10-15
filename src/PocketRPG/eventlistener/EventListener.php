f39544aa24ed766b48808a57674a81eea828bf99
Blabla <st.ten.veldhuis@gmail.com>
2016-10-15 15:20:17 +0200

    Update EventListener.php

--- src/PocketRPG/eventlistener/EventListener.php
+++ src/PocketRPG/eventlistener/EventListener.php
@@ -63,10 +63,6 @@ class EventListener extends Main implements Listener {
      return $this->plugin;
   }
 
-  public function getServer() {
-    return $this->plugin->getServer();
-  }
-
   public function onQuit (PlayerQuitEvent $event) {
     $p = $event->getPlayer ();
     $party = new Config ($this->getDataFolder () . "plugins/PocketRPG/party/" . $p->getName () . ".yml");
