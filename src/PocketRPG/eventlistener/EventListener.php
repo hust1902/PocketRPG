89206be4bac1570627510230bb8c3a4e1ed6133b
Blabla <st.ten.veldhuis@gmail.com>
2016-10-18 10:42:30 +0200

    Still nothing...

--- src/PocketRPG/eventlistener/EventListener.php
+++ src/PocketRPG/eventlistener/EventListener.php
@@ -343,7 +343,7 @@ class EventListener extends Main implements Listener {
     }
   }
   
-  public function onExpChange(PlayerExperienceChangeEvent $event) {
+  \*public function onExpChange(PlayerExperienceChangeEvent $event) {
     $p = $event->getPlayer();
     if($p instanceof Player && $p->getLevel()->getName() == $this->getOwner()->config->get("RPGworld")) {
       if($this->getOwner()->playerclass->get($p->getName()) === "mage" && $event->getExpLevel() >= 10) {
@@ -387,10 +387,8 @@ class EventListener extends Main implements Listener {
           $p->sendMessage(TF::GREEN . "You have unlocked the strength spell!");
         }
       }
-    } //elseif($p instanceof Player && $event->getExp() >= 370) {
-      //start for special weapons
-    //} 
-  }
+    } 
+  } *\
   
   public function onBreak(BlockBreakEvent $event) {
     $p = $event->getPlayer();
